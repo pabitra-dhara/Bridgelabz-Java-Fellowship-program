f6dadc7d6df720f4305005f0871d2205113021dc
Pabitra Dhara <pabitradharardh@gmail.com>
2026-01-29 15:11:34 +0530

    fix class file

--- Core Programming Basics/TypeConversions.java
+++ Core Programming Basics/TypeConversions.java
@@ -1,19 +1,16 @@
-import java.util.Scanner;
-public class TypeConversions{
-public static void main(String args[]){
-int a=100;
-double b=a;
-System.out.println("Integer to Double:"+a+"=>"+b);
-long c=a;
-System.out.println("Integer to Long:"+a+"=>"+c);
+public class TypeConversions {
+    public static void main(String args[]) {
+        int a = 100;
+        double b = a;
+        System.out.println("Integer to Double:" + a + "=>" + b);
+        long c = a;
+        System.out.println("Integer to Long:" + a + "=>" + c);
 
-double k=10.56;
-int d=(int)k;
-System.out.println("Double to Integer:"+k+"=>"+d);
-long p=58746;
-int e=(int)p;
-System.out.println("long to Integer:"+p+"=>"+e);
+        double k = 10.56;
+        int d = (int) k;
+        System.out.println("Double to Integer:" + k + "=>" + d);
+        long p = 58746;
+        int e = (int) p;
+        System.out.println("long to Integer:" + p + "=>" + e);
+    }
 }
-}
-
-
